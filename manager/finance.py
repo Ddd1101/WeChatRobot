54f9fd876ed75448211047782f155fb86b7112e7
Ddd <752042257@qq.com>
2024-04-09 03:44:12 +0800

    WIP: "返款"参数整理

--- manager/finance.py
+++ manager/finance.py
@@ -36,6 +36,35 @@ class Transaction:
         print(self.TAG, msgContentList)
         if msgContentList[0] == "返款":
             print(user_id[msg.sender]["name"])
+            self.refund(msgContentList, msg)
+
+    def refund(self, msgContentList, msg: WxMsg):
+        # 金额
+        amount = float(msgContentList[1])
+        # 付款方
+        payer = user_id[msg.sender]["name"]
+        # 收款方
+        payee = msgContentList[2]
+        if payee == "金" or payee == "国金" or payee == "侯国金":
+            payee == "侯国金"
+        # 备注
+        remark = ""
+        if len(msgContentList) >= 4:
+            remark = msgContentList[3]
+        # 拼接请求参数
+        params = {
+            "data": {
+                "create_time": "",
+                "company": "万盈",
+                "payer": payer,
+                "payee": payee,
+                "amount": float(msgContentList[1]),
+                "issue": "返款",
+                "remark": remark,
+            }
+        }
+
+        print(params)
 
 
 if __name__ == "__main__":
